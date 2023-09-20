a0a23f2f193f1596fdb9ee49a5c1b8ff34a428ae
yudayuda78 <ariefyudha78@gmail.com>
2023-09-20 22:26:32 +0700

    edit tampilan UI mobile version

--- resources/views/landingpage/landingpage.blade.php
+++ resources/views/landingpage/landingpage.blade.php
@@ -12,9 +12,10 @@
                         <h1>Semua yang Anda Butuhkan dalam Pembelajaran <br>ada di sini</h1>
                         <button type="button" class="head1-button btn btn-danger rounded-pill "><a href="/koleksi"
                                 class="text-light text-center">Kunjungi</a></button>
-                        
+
                     </div>
                     <img class="illust1" src="asset/illust1.webp" alt="">
+                    <img class="illust2" src="asset/hp123.webp" alt="">
                 </div>
             </div>
         </div>
@@ -42,15 +43,18 @@
                         <div class="ayam">
                             <div class="your-class">
                                 <div class="myclass">
-                                    <div class="wrapper-imgground"><img src="asset/modulajar.webp" alt="" class="imground"></div>
+                                    <div class="wrapper-imgground"><img src="asset/modulajar.webp" alt=""
+                                            class="imground"></div>
                                     <p><a href="" class="text-light">Worksheet</a></p>
                                 </div>
                                 <div class="myclass">
-                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt="" class="imground"></div>
+                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt=""
+                                            class="imground"></div>
                                     <p><a href="" class="text-light">Workbook</a></p>
                                 </div>
                                 <div class="myclass">
-                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt="" class="imground"></div>
+                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt=""
+                                            class="imground"></div>
                                     <p><a href="" class="text-light">Activity book</a></p>
                                 </div>
                                 <div class="myclass" class="imground">
@@ -82,35 +86,43 @@
                                     <p><a href="" class="text-light">Hands on activities/ DIY</a></p>
                                 </div>
                                 <div class="myclass" class="imground">
-                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt=""></div>
+                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt="">
+                                    </div>
                                     <p><a href="" class="text-light">Stories book</a></p>
                                 </div>
                                 <div class="myclass" class="imground">
-                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt=""></div>
+                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt="">
+                                    </div>
                                     <p><a href="" class="text-light">Online exercises</a></p>
                                 </div>
                                 <div class="myclass" class="imground">
-                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt=""></div>
+                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt="">
+                                    </div>
                                     <p><a href="" class="text-light">Modul proyek</a></p>
                                 </div>
                                 <div class="myclass" class="imground">
-                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt=""></div>
+                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt="">
+                                    </div>
                                     <p><a href="" class="text-light">Modul ajar</a></p>
                                 </div>
                                 <div class="myclass" class="imground">
-                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt=""></div>
+                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt="">
+                                    </div>
                                     <p><a href="" class="text-light">Diklat dan event</a></p>
                                 </div>
                                 <div class="myclass" class="imground">
-                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt=""></div>
+                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt="">
+                                    </div>
                                     <p><a href="" class="text-light">Diklat dan event</a></p>
                                 </div>
                                 <div class="myclass" class="imground">
-                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt=""></div>
+                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt="">
+                                    </div>
                                     <p><a href="" class="text-light">Ecourse</a></p>
                                 </div>
                                 <div class="myclass" class="imground">
-                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt=""></div>
+                                    <div class="wrapper-imgground"><img src="asset/storiesbook.webp" alt="">
+                                    </div>
                                     <p><a href="" class="text-light">Rapor proyek</a></p>
                                 </div>
                             </div>
@@ -137,35 +149,45 @@
                             </div>
                         </div>
                     </div>
-                    <img src="" alt="">
-                </div>
-                <div class="carousel-item">
-                       <div class="container">
-                        <div class="container item-car1">
-                            <div class="item1">
-                                <h1 class="text-dark"> Workbook</h1>
-                                <p>Workbook adalah sebuah buku yang berisi kumpulan kegiatan pembelajaran yang dirancang khusus untuk meningkatkan pemahamana dan keterampilan siswa
-                                </p>
-                                <button type="button" class="head1-button btn btn-danger rounded-pill "><a
-                                        href="/koleksi" class="text-light text-center">Kunjungi</a></button>
-                            </div>
-                        </div>
+                    <div class="imgwrapper">
+                        <img src="asset/busy.webp" alt="">
                     </div>
-                    <img src="" alt="">
                 </div>
                 <div class="carousel-item">
                     <div class="container">
                         <div class="container item-car1">
                             <div class="item1">
-                                <h1 class="text-dark"> Activity book</h1>
-                                <p>Activity book adalah buku yang dirancang khusus untuk memberikan pengalaman belajar yang interaktif dan praktis kepada siswa.
-                                </p>
-                                <button type="button " class="head1-button btn btn-danger rounded-pill "><a
+                                <h1 class="text-dark"> DIY</h1>
+                                <p>Worksheet Printable adalah alat pendidikan yang praktis dan mudah digunakan yang
+                                    dirancang
+                                    untuk
+                                    mendukung proses pembelajaran di berbagai tingkatan pendidikan</p>
+                                <button type="button" class="head1-button btn btn-danger rounded-pill "><a
                                         href="/koleksi" class="text-light text-center">Kunjungi</a></button>
                             </div>
                         </div>
                     </div>
-                    <img src="" alt="">
+                    <div class="imgwrapper">
+                        <img src="asset/Diy11.webp" alt="">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="container">
+                        <div class="container item-car1">
+                            <div class="item1">
+                                <h1 class="text-dark"> DIY</h1>
+                                <p>Worksheet Printable adalah alat pendidikan yang praktis dan mudah digunakan yang
+                                    dirancang
+                                    untuk
+                                    mendukung proses pembelajaran di berbagai tingkatan pendidikan</p>
+                                <button type="button" class="head1-button btn btn-danger rounded-pill "><a
+                                        href="/koleksi" class="text-light text-center">Kunjungi</a></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="imgwrapper">
+                        <img src="asset/storiesbook1.webp" alt="">
+                    </div>
                 </div>
             </div>
             {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
@@ -179,69 +201,120 @@
             </button>
         </div>
 
-
-
-
-
-
-
-
-        {{-- <div id="carouselExampleFade" class="carousel slide carousel-fade">
-
-
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <h1> Worksheet</h1>
-                    <p>Worksheet Printable adalah alat pendidikan yang praktis dan mudah digunakan yang dirancang untuk
-                        mendukung proses pembelajaran di berbagai tingkatan pendidikan</p>
-                    <button type="button" class="head1-button btn btn-danger rounded-pill "><a href="/koleksi"
-                            class="text-light text-center">Kunjungi</a></button>
-                </div>
-                <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <div class="head4">
+        <div class="head5">
+            <div class="container">
                 <div class="container">
-                    <div class="container">
-                        {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-                            data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button> --}}
-        {{-- <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-                            data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
+                    <div class="head5-content">
+                        <h1><span class="belajarbersama"></span></h1>
+                        <p class="head5p">TicyKit adalah Platform pendidikan yang menyediakan alat pembelajaran untuk para
+                            pendidik, siswa,
+                            dan orang tua yang mencari beragam kebutuhan untuk belajar, mengajar yang inovatif dan
+                            berkualitas</p>
+                        <div class="flex-grid">
+                            <div class="grid-container">
+                                <div class="grid-item">
+                                    <div class="img-wrap">
+                                        <img src="asset/bebasongkir.webp" alt="">
+                                    </div>
+                                    <div class="content-belajar-wrap">
+                                        <h2>Bebas Ongkir</h2>
+                                        <p>Tinggal Klik, Download & Print</p>
+                                    </div>
+                                </div>
+                                <div class="grid-item">
+                                    <div class="img-wrap">
+                                        <img src="" alt="">
+                                    </div>
+                                    <div class="content-belajar-wrap">
+                                        <h2>Praktis dan Efisien</h2>
+                                        <p>Produk digital tanpa biaya pengiriman</p>
+                                    </div>
+                                </div>
+                                <div class="grid-item">
+                                    <div class="img-wrap">
+                                        <img src="" alt="">
+                                    </div>
+                                    <div class="content-belajar-wrap">
+                                        <h2>Materi Sesuai Usia</h2>
+                                        <p>Dirancang agar cocok dengan usia siswa</p>
+                                    </div>
+                                </div>
+                                <div class="grid-item">
+                                    <div class="img-wrap">
+                                        <img src="" alt="">
+                                    </div>
+                                    <div class="content-belajar-wrap">
+                                        <h2>Desain Menarik</h2>
+                                        <p>Warna-warni yang ceria dan gambar yang menarik</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
 
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div>
 
-    </div>
-
-
-
-    <div class="head5">
-        <div class="container">
+        <div class="head6">
             <div class="container">
+                <div class="container">
+                    <div class="head6-content">
+                        <div class="head6-class">
+                            <div class="head6-myclass">
+                                <div class="head6wrapper-imgground"><img src="" alt=""
+                                        class="head6imground"></div>
+                                <p><a href="" class="text-light">Worksheet</a></p>
+                            </div>
+                            <div class="head6-myclass">
+                                <div class="head6wrapper-imgground"><img src="" alt=""
+                                        class="head6imground"></div>
+                                <p><a href="" class="text-light">Worksheet</a></p>
+                            </div>
+                            <div class="head6-myclass">
+                                <div class="head6wrapper-imgground"><img src="" alt=""
+                                        class="head6imground"></div>
+                                <p><a href="" class="text-light">Worksheet</a></p>
+                            </div>
+                            <div class="head6-myclass">
+                                <div class="head6wrapper-imgground"><img src="" alt=""
+                                        class="head6imground"></div>
+                                <p><a href="" class="text-light">Worksheet</a></p>
+                            </div>
+                            <div class="head6-myclass">
+                                <div class="head6wrapper-imgground"><img src="" alt=""
+                                        class="head6imground"></div>
+                                <p><a href="" class="text-light">Worksheet</a></p>
+                            </div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="head6">
-        <div class="container">
-            <div class="container">
 
-            </div>
-        </div>
-    </div>
+
+
+
+
 
     <div class="head7">
         <div class="container">
@@ -273,6 +346,10 @@
         </div>
     </div>
 
+    <div class="10">
+
+    </div>
+
 
 
     </div>
@@ -292,6 +369,13 @@
             loop: true
         });
 
+        var typed = new Typed(".belajarbersama", {
+            strings: ["Mengapa Belajar Bersama TinyKit?"],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true
+        });
+
         window.addEventListener('scroll', function() {
             const parallax = document.querySelector('.head1');
             let scrollPosition = window.pageYOffset;
@@ -303,7 +387,7 @@
             const head1p = document.querySelector('.head1-content');
             const head2p = document.querySelector('.head2p');
             const illust1 = document.querySelector('.illust1');
-            
+
             illust1.classList.add('muncul');
             head1p.classList.add('muncul');
             head2p.classList.add('muncul');
@@ -312,9 +396,45 @@
 
         $(document).ready(function() {
             $('.your-class').slick({
+                infinite: true,
+                speed: 300,
+                slidesToShow: 5,
+                slidesToScroll: 3,
+                responsive: [{
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+                    // You can unslick at a given breakpoint now by adding:
+                    // settings: "unslick"
+                    // instead of a settings object
+                ]
+            });
+        });
+
+        $(document).ready(function() {
+            $('.head6-class').slick({
                 arrows: true,
                 infinite: true,
-                slidesToShow: 5,
+                slidesToShow: 3,
                 slidesToScroll: 3
             });
         });
